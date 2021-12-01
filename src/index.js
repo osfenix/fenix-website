c30cb62b72a1613c19d086f7f29d1ee4038706a9
Matheus Justino <matheussjusttino@gmail.com>
2021-11-30 22:10:04 -0300

    feat: responsive header

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let hamburguer = document.getElementById("hamburguer")
+let nav = document.querySelector("nav");
+console.log(nav);
+
+hamburguer.addEventListener("click", e => {
+    if (nav.classList.contains("nav-displayed"))
+        nav.classList.remove("nav-displayed");
+    else
+        nav.classList.add("nav-displayed");
+})

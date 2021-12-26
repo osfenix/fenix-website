328499ae9fd0c30434e55271868a67986cea9ef3
adviuriaraujo <85589853+adviuriaraujo@users.noreply.github.com>
2021-12-26 11:03:19 -0300

    refactor: refatora functions do index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,29 @@ AOS.init();
 
 let hamburguer = document.getElementById("hamburguer")
 let nav = document.querySelector("nav");
-console.log(nav);
+let header_links = Array.from(document.querySelectorAll(".nav_item"));
+header_links.push(hamburguer);
 
-hamburguer.addEventListener("click", e => {
-    if (nav.classList.contains("nav-displayed"))
-        nav.classList.remove("nav-displayed");
-    else
-        nav.classList.add("nav-displayed");
+console.log(nav);
+console.log(header_links);
+
+header_links.forEach(i => {
+    i.addEventListener("click", e => {
+        if (nav.classList.contains("nav-displayed"))
+            nav.classList.remove("nav-displayed");
+        else
+            nav.classList.add("nav-displayed");
+    })
 })
 
+
 //Redirects to index after sending email on contact form
-function redirect() {
+/*function redirect() {
     let back_btn = document.getElementById("sent_back_btn");
     back_btn= window.location.assign("./index.html")
-}
+}*/
 
+let back_btn = document.getElementById("sent_back_btn");
+if (back_btn) {back_btn.addEventListener("click", e => {
+    window.location.assign("./index.html")
+})};

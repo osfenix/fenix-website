525d8c4bba02a26bc2dc55bdf4d821dcc0d8f3f8
adviuriaraujo <85589853+adviuriaraujo@users.noreply.github.com>
2021-12-29 09:03:37 -0300

    refactor: altera nomes de variavel para camelCase

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ AOS.init();
 
 let hamburguer = document.getElementById("hamburguer")
 let nav = document.querySelector("nav");
-let header_links = Array.from(document.querySelectorAll(".nav_item"));
-header_links.push(hamburguer);
+let headerLinks = Array.from(document.querySelectorAll(".nav_item"));
+headerLinks.push(hamburguer);
 
 console.log(nav);
-console.log(header_links);
+console.log(headerLinks);
 
-header_links.forEach(i => {
+headerLinks.forEach(i => {
     i.addEventListener("click", e => {
         if (nav.classList.contains("nav-displayed"))
             nav.classList.remove("nav-displayed");
@@ -20,12 +20,8 @@ header_links.forEach(i => {
 
 
 //Redirects to index after sending email on contact form
-/*function redirect() {
-    let back_btn = document.getElementById("sent_back_btn");
-    back_btn= window.location.assign("./index.html")
-}*/
 
-let back_btn = document.getElementById("sent_back_btn");
-if (back_btn) {back_btn.addEventListener("click", e => {
+let backBtn = document.getElementById("sent_back_btn");
+if (backBtn) {backBtn.addEventListener("click", e => {
     window.location.assign("./index.html")
 })};
